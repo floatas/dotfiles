fd9272718061a8a912b39c7a2881b1e55c2f63bf
Floatas <floatas@outlook.com>
2015-03-02 23:44:22 +0200

    bundle repositories

---
+++ git_bundle_repos.cmd
@@ -0,0 +1,11 @@
+git clone https://github.com/xolox/vim-misc
+git clone https://github.com/mhinz/vim-startify
+git clone https://github.com/xolox/vim-session
+git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/tpope/vim-dispatch.git
+git clone https://github.com/Chiel92/vim-autoformat
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/OmniSharp/omnisharp-vim.git
+git clone https://github.com/nosami/omnisharp.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/kien/ctrlp.vim.git
